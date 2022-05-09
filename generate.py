b1208bff5195ee3aed04771378474f5a85d5753a
Nick Ivanov <sm230@pm.me>
2022-05-09 13:20:00 -0400

    Mon May  9 01:20:00 PM EDT 2022

--- generate.py
+++ generate.py
@@ -4,6 +4,8 @@ import csv
 with open('raw_data.csv') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
-        print('||'.join(row))
+        print(row[0])
         print("----")
+        with open(row[0], "w") as stmfile:
+            stmfile.write("<!DOCTYPE html>\n<html><body>Blah.</body></html>")
     
